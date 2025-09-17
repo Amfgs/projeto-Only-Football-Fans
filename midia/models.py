2fa1c153fb24af490f25df68dbc277c3e633e980
FialhoMath <mgfm@cesar.school>
2025-09-17 16:12:28 -0300

    ultimo commit do dia: adicionei linhas na url e settings importantes para guardar e reproduzir as midias corretamente

--- midia/models.py
+++ midia/models.py
@@ -10,4 +10,27 @@ class Definicao(models.Model):
     def __str__(self):
         return f"{self.jogo} - {self.descricao}"
     
+class Imagem(models.Model):
+    definicao = models.ForeignKey(Definicao, on_delete=models.CASCADE, related_name="Imagens")
+    arquivo = models.ImageField(upload_to="Imagens/")
+    criado_em = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return f"Imagem da partida {self.definicao.id}"
+    
+class Video(models.Model):
+    definicao = models.ForeignKey(Definicao, on_delete=models.CASCADE, related_name="videos")
+    arquivo = models.FileField(upload_to="videos/")
+    criado_em = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Vídeo da partida {self.definicao.id}"
+
+
+class Audio(models.Model):
+    definicao = models.ForeignKey(Definicao, on_delete=models.CASCADE, related_name="audios")
+    arquivo = models.FileField(upload_to="audios/")
+    criado_em = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Áudio da partida {self.definicao.id}"
