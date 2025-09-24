f0993ebcbd545f921a9b4f1dcb50957e4406bf00
FialhoMath <mgfm@cesar.school>
2025-09-24 11:56:46 -0300

    HistoricoPartida/models.py/partidas linkado ao models.py/midias

--- midia/models.py
+++ midia/models.py
@@ -1,31 +1,33 @@
 from django.db import models
 from django.utils import timezone
+from partidas.models import HistoricoPartida
+
 # Create your models here.
 
 class Definicao(models.Model):
-    jogo = models.CharField(max_length=200) #Depois mudarei para ser somente a referência do jogo ja colocado no app de partida
-    descricao = models.CharField(blank=True) #Pode ser a descricao da partida tambem vindo do app de partida ou so a descricao da midia ou ambas marcam
+    jogo = models.ForeignKey(HistoricoPartida, on_delete=models.CASCADE, related_name="midias")
+    descricao = models.CharField(max_length=200, blank=True)  # Pode ser descrição da midia ou da partida
     criado_em = models.DateField(default=timezone.now)
 
     def __str__(self):
         return f"{self.jogo} - {self.descricao}"
+
     
 class Imagem(models.Model):
-    definicao = models.ForeignKey(Definicao, on_delete=models.CASCADE, related_name="Imagens")
-    arquivo = models.ImageField(upload_to="Imagens/")
+    definicao = models.ForeignKey(Definicao, on_delete=models.CASCADE, related_name="imagens")
+    arquivo = models.ImageField(upload_to="imagens/")
     criado_em = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"Imagem da partida {self.definicao.id}"
-    
+        return f"Imagem da partida {self.definicao.jogo.id}"
+
 class Video(models.Model):
     definicao = models.ForeignKey(Definicao, on_delete=models.CASCADE, related_name="videos")
     arquivo = models.FileField(upload_to="videos/")
     criado_em = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"Vídeo da partida {self.definicao.id}"
-
+        return f"Vídeo da partida {self.definicao.jogo.id}"
 
 class Audio(models.Model):
     definicao = models.ForeignKey(Definicao, on_delete=models.CASCADE, related_name="audios")
@@ -33,4 +35,4 @@ class Audio(models.Model):
     criado_em = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"Áudio da partida {self.definicao.id}"
+        return f"Áudio da partida {self.definicao.jogo.id}"
