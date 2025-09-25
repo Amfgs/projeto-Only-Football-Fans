d23e4fa7cf0c595c31ed40984c5c1c934c5c8ef8
Arthur Moury <amfgs@cesar.school>
2025-09-25 20:59:59 -0300

    Processo

--- OFF/urls.py
+++ OFF/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,5 +9,6 @@ urlpatterns = [
     path('emocao/', include('emocao.urls')),  # inclui as URLs do app emocao
     path('partidas/', include('partidas.urls')),  # adiciona o app partidas
     path('accounts/', include('usuarios.urls')),  # adiciona o app usuarios
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
 
