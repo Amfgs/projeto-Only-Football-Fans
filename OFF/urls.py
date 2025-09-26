bf00bedac9c8b14de4879fe8c4b1f28c56363445
Arthur Moury <amfgs@cesar.school>
2025-09-25 21:10:16 -0300

    Forçando o usuário a começar pelo login

--- OFF/urls.py
+++ OFF/urls.py
@@ -3,12 +3,18 @@ from django.urls import path, include
 from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
 
+def home(request):
+    return redirect('login')
+
 urlpatterns = [
+    path('', home, name='home'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='usuarios/login.html'  # <-- apontando para o template do app usuarios
+    ), name='login'),
     path('admin/', admin.site.urls),
     path('', lambda request: redirect('/emocao/')),  # redireciona para a página inicial do app emocao
     path('emocao/', include('emocao.urls')),  # inclui as URLs do app emocao
     path('partidas/', include('partidas.urls')),  # adiciona o app partidas
     path('accounts/', include('usuarios.urls')),  # adiciona o app usuarios
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
 
