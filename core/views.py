00fe0507154050f325d49c36996ff17333651ffc
Guilherme Silvestre <lgsilvestre23@gmail.com>
2025-10-17 14:23:16 -0300

    Remove unused import for User model

--- core/views.py
+++ core/views.py
@@ -4,7 +4,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import HttpResponse
-from django.contrib.auth.models import User
 from django.db.models import Q 
 
 from .models import (
