ee7f0beda67abfcfdea4a4a16527adb53cc890ad
Arthur <amfgs@cesar.school>
2025-10-20 19:16:40 -0300

    Alterando arquivos .cy.js para rodar os testes

--- teste/cypress/e2e/avaliar_torcida.cy.js
+++ teste/cypress/e2e/avaliar_torcida.cy.js
@@ -3,20 +3,19 @@ describe('Avaliar Torcida', () => {
     cy.login()
   })
 
-  it('Deve avaliar torcida da primeira partida', () => {
-    cy.visit('http://127.0.0.1:8000/partidas/avaliar_torcida/5/')
+  it('Deve avaliar torcida dos times 1 e 2', () => {
+    cy.visit('http://127.0.0.1:8000/partidas/avaliar_torcida/1/')
 
     // Opção 1: usando o input escondido
     cy.get('input[name="emocao"][value="4"]').check({ force: true })
     cy.get('input[name="presenca"][value="5"]').check({ force: true })
-
-    // Opção 2 (se quiser usar label):
-    // cy.get('label[for="emocao_4"]').click()
-    // cy.get('label[for="presenca_5"]').click()
-
     cy.get('textarea[name="comentario"]').type('Torcida animada demais!')
     cy.contains('button', 'Mandar o Review da Torcida').click()
+  
 
-    cy.contains('Mandar o Review da Torcida') // Ajustar depois se houver redirecionamento
+    cy.get('input[name="emocao"][value="4"]').check({ force: true })
+    cy.get('input[name="presenca"][value="5"]').check({ force: true })
+    cy.get('textarea[name="comentario"]').type('Torcida fraca demais!')
+    cy.contains('button', 'Mandar o Review da Torcida').click()
   })
 })
--- teste/cypress/support/commands.js
+++ teste/cypress/support/commands.js
@@ -26,8 +26,8 @@
 
 Cypress.Commands.add('login', () => {
   cy.visit('http://127.0.0.1:8000/login/')
-  cy.get('input[name="username"]').type('Jaime')
-  cy.get('input[name="password"]').type('Jaime**12')
+  cy.get('input[name="username"]').type('novoUsuario4')
+  cy.get('input[name="password"]').type('senha123')
   cy.get('button[type="submit"]').click()
   cy.url().should('eq', 'http://127.0.0.1:8000/')
 })
