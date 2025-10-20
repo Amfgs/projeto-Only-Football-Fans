9c901f0d0c64808d3e165a8e152f7aaaf7d8fa92
Arthur <amfgs@cesar.school>
2025-10-19 21:55:13 -0300

    Mudando id pra filmar o teste

--- teste/cypress/e2e/avaliar_partida.cy.js
+++ teste/cypress/e2e/avaliar_partida.cy.js
@@ -4,7 +4,7 @@ describe('Avaliar Partida', () => {
   })
 
   it('Deve avaliar a primeira partida da lista', () => {
-    cy.visit('http://127.0.0.1:8000/partidas/avaliar/5/') // Atualizar ID dinamicamente se quiser
+    cy.visit('http://127.0.0.1:8000/partidas/avaliar/7/') // Atualizar ID dinamicamente se quiser
     cy.get('input[name="nota"]').type('4')
     cy.get('input[name="melhor_jogador"]').type('Messi')
     cy.get('input[name="pior_jogador"]').type('Maradona')
