ecfa17db7899365b010fd436d5dc1e6a798f8be0
Arthur <amfgs@cesar.school>
2025-11-09 15:27:55 -0300

    Corrigindo settings.py

--- OFF/settings.py
+++ OFF/settings.py
@@ -126,13 +126,14 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# STATIC_URL = "static/"
 STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-# ======================
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 # MEDIA FILES (LOCAL + AZURE)
 # ======================
 
